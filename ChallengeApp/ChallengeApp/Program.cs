d5e4cf06f618998caad6f0ba926843dd7bf43766
DariuszKoziara <Dariusz.Koziara1985@gmail.com>
2024-02-24 21:36:48 +0100

    Day 4th

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var age = 35;
+var sex = "woman";
+
+if (sex == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "man" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
